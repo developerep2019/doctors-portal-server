4077691258b01817adb7f28c909c69ddbdb874da
developerep2019 <developerep2019@gmail.com>
2020-10-26 19:00:43 +0600

    server half done

--- server.js
+++ server.js
@@ -5,20 +5,43 @@ const MongoClient = require('mongodb').MongoClient;
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
+require('dotenv').config();
 
 const port = 5000;
-const uri = "mongodb+srv://<username>:<password>@cluster0.oskdg.mongodb.net/<dbname>?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.oskdg.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
-app.get('/' , (req, res) => {
-    res.send(['database' , 'working' , 'properly']);
+app.get('/', (req, res) => {
+  res.send(['database', 'working', 'properly']);
 });
 
 
-const client = new MongoClient(uri, { useNewUrlParser: true,  useUnifiedTopology: true });
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-  const collection = client.db("test").collection("devices");
+  const appointmentCollection = client.db("doctorsPortal").collection("appointments");
+  app.post('/addAppointment', (req, res) => {
+    const appointment = req.body;
+    appointmentCollection.insertOne(appointment)
+      .then(result => {
+        res.send(result.insertedCount > 0)
+      })
+  });
+
+  app.get('/appointments' , (req, res) => {
+    appointmentCollection.find({})
+    .toArray((err, documents) => {
+      res.send(documents)
+    })
+  })
   
-  client.close();
+  app.post('/appointmentsByDate', (req, res) => {
+    const date = req.body;
+    appointmentCollection.find({ date: date.date })
+      .toArray((err, documents) => {
+        res.send(documents)
+      })
+  });
+
+
 });
 
 
